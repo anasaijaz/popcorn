f81ad1f5a47b458a946a7baf43ccbd2d4b36a104
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-03-28 16:06:44 +0530

    todo yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+    def new
+        @user = User.new
+    end
     def index
         @user = current_user   
     end
