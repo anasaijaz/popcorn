cd99761b93dabc8d89a1778723847cc3b4b01d61
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-03-29 19:06:30 +0530

    some more progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+    protect_from_forgery prepend: true
     before_action :authenticate_user!
     def index
+        
         @user = current_user   
     end
 
